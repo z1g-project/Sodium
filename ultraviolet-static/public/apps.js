cddc070106f2f539dda91e16319e8d3e408a1e98
John Glynn <playingallday383@gmail.com>
2023-06-27 17:06:34 -0400

    Finished the apps Page

--- ultraviolet-static/public/apps.js
+++ ultraviolet-static/public/apps.js
@@ -1,18 +1,7 @@
-"use strict";
-
-/**
- * @type {HTMLButtonElement}
- */
-const displus = document.getElementById("displus");
-
-displus.addEventListener("click", async () => {
-  try {
-    await registerSW();
-  } catch (err) {
-    console.error("Failed to register service worker.", err);
-    return;
+function loadApp(url) {
+    // Perform any necessary actions before loading the app
+  
+    // Redirect to the specified URL
+    window.location.href = url;
   }
-
-  const url = "https://www.openai.com"; // Replace with the desired URL
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-});
+  
