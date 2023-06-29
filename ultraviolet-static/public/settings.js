5746c812529482d759c57f1231eb4119247d5883
John Glynn <playingallday383@gmail.com>
2023-06-29 13:59:01 -0400

    Finish Stealth Features

--- ultraviolet-static/public/settings.js
+++ ultraviolet-static/public/settings.js
@@ -1,154 +1,73 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Load and set saved settings
-  loadSavedSettings();
-
-  // Add event listeners for save and reset buttons
-  document.getElementById('tab-cloak-icon-save-btn').addEventListener('click', saveTabCloakIcon);
-  document.getElementById('tab-cloak-icon-reset-btn').addEventListener('click', resetTabCloakIcon);
-  document.getElementById('cloak-save-btn').addEventListener('click', saveCloakValue);
-  document.getElementById('cloak-reset-btn').addEventListener('click', resetCloakValue);
-  document.getElementById('css-modification-save-btn').addEventListener('click', saveCSSModification);
-  document.getElementById('css-modification-reset-btn').addEventListener('click', resetCSSModification);
-});
-
-function loadSavedSettings() {
-  // Load Tab Cloak Icon URL
-  const tabCloakIconURL = loadSetting('tabCloakIconURL');
-  if (tabCloakIconURL) {
-    document.getElementById('tab-cloak-icon-input').value = tabCloakIconURL;
-    document.getElementById('tab-cloak-icon-value').textContent = tabCloakIconURL;
-    applyTabCloakIcon(tabCloakIconURL);
-  }
-
-  // Load Cloak value
-  const cloakValue = loadSetting('cloakValue');
-  if (cloakValue) {
-    document.getElementById('cloak-input').value = cloakValue;
-    applyCloakTitle(cloakValue);
-  }
-
-  // Load CSS modification
-  const cssModification = loadSetting('cssModification');
-  if (cssModification) {
-    document.getElementById('css-modification-input').value = cssModification;
-    applyCSSModification(cssModification);
-  }
-
-  function applyCloakTitle(cloakTitle) {
-  // Set the Cloak title on all pages
-  document.title = cloakTitle || 'Sodium';
-  }
-}
-
-function applyTabSettings(tabCloakIconURL, tabTitle) {
-  // Apply Tab Cloak Icon URL to all pages
-  const existingIcon = document.getElementById('custom-tab-cloak-icon');
-  if (tabCloakIconURL) {
-    if (existingIcon) {
-      existingIcon.href = tabCloakIconURL;
-    } else {
-      const newIcon = document.createElement('link');
-      newIcon.id = 'custom-tab-cloak-icon';
-      newIcon.rel = 'icon';
-      newIcon.href = tabCloakIconURL;
-      document.head.appendChild(newIcon);
+// Function to get the value of a cookie by name
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(name + '=')) {
+        return cookie.substring(name.length + 1);
+      }
     }
-  } else {
-    if (existingIcon) {
-      existingIcon.remove();
+    return null;
+  }
+  
+  // Function to set the value of a cookie
+  function setCookie(name, value, expirationDays = 365, path = '/') {
+    const date = new Date();
+    date.setDate(date.getDate() + expirationDays);
+    const expires = date.toUTCString();
+    document.cookie = `${name}=${value}; expires=${expires}; path=${path}`;
+  }
+  
+  // Function to load and apply the saved settings
+  function loadSavedSettings() {
+    // Load and set the title
+    const title = getCookie('websiteTitle');
+    if (title) {
+      document.title = title;
+      const websiteTitleElem = document.getElementById('website-title');
+      if (websiteTitleElem) {
+        websiteTitleElem.textContent = title;
+      }
+    }
+  
+    // Load and set the icon
+    const icon = getCookie('websiteIcon');
+    if (icon) {
+      const favicon = document.getElementById('favicon');
+      if (favicon) {
+        favicon.href = icon;
+        favicon.setAttribute('type', 'image/png');
+      }
     }
   }
-
-  // Apply Tab Title to all pages
-  const pageTitle = document.querySelector('title');
-  if (pageTitle) {
-    pageTitle.innerText = tabTitle || 'Sodium';
-  }
-}
-
-function loadSetting(key) {
-  return localStorage.getItem(key);
-}
-
-function saveSetting(key, value) {
-  localStorage.setItem(key, value);
-}
-
-function saveTabCloakIcon() {
-  const tabCloakIconURLInput = document.getElementById('tab-cloak-icon-input');
-  const newTabCloakIconURL = tabCloakIconURLInput.value;
-  saveSetting('tabCloakIconURL', newTabCloakIconURL);
-  document.getElementById('tab-cloak-icon-value').textContent = newTabCloakIconURL;
-  applyTabCloakIcon(newTabCloakIconURL);
-}
-
-function resetTabCloakIcon() {
-  const tabCloakIconURLInput = document.getElementById('tab-cloak-icon-input');
-  tabCloakIconURLInput.value = '';
-  saveSetting('tabCloakIconURL', '');
-  document.getElementById('tab-cloak-icon-value').textContent = '';
-  applyTabCloakIcon('');
-}
-
-function applyTabCloakIcon(iconURL) {
-  const existingIcon = document.getElementById('custom-tab-cloak-icon');
-  if (iconURL) {
-    if (existingIcon) {
-      existingIcon.href = iconURL;
-    } else {
-      const newIcon = document.createElement('link');
-      newIcon.id = 'custom-tab-cloak-icon';
-      newIcon.rel = 'icon';
-      newIcon.href = iconURL;
-      document.head.appendChild(newIcon);
+  
+  // Function to save the website settings
+  function saveWebsiteSettings() {
+    const titleInput = document.getElementById('title-input');
+    const iconInput = document.getElementById('icon-input');
+  
+    const title = titleInput.value;
+    const icon = iconInput.value;
+  
+    // Set the title and icon as cookies
+    setCookie('websiteTitle', title);
+    setCookie('websiteIcon', icon);
+  
+    // Apply the saved settings
+    document.title = title;
+    const websiteTitleElem = document.getElementById('website-title');
+    if (websiteTitleElem) {
+      websiteTitleElem.textContent = title;
     }
-  } else {
-    if (existingIcon) {
-      existingIcon.remove();
+    const favicon = document.getElementById('favicon');
+    if (favicon) {
+      favicon.href = icon;
+      favicon.setAttribute('type', 'image/png');
     }
   }
-}
-
-function saveCloakValue() {
-  const cloakValueInput = document.getElementById('cloak-input');
-  const newCloakValue = cloakValueInput.value;
-  saveSetting('cloakValue', newCloakValue);
-}
-
-function resetCloakValue() {
-  const cloakValueInput = document.getElementById('cloak-input');
-  cloakValueInput.value = '';
-  saveSetting('cloakValue', '');
-}
-
-function saveCSSModification() {
-  const cssModificationInput = document.getElementById('css-modification-input');
-  const newCSSModification = cssModificationInput.value;
-  saveSetting('cssModification', newCSSModification);
-  applyCSSModification(newCSSModification);
-}
-
-function resetCSSModification() {
-  const cssModificationInput = document.getElementById('css-modification-input');
-  cssModificationInput.value = '';
-  saveSetting('cssModification', '');
-  applyCSSModification('');
-}
-
-function applyCSSModification(cssCode) {
-  const existingStyleTag = document.getElementById('custom-css-modification');
-  if (cssCode) {
-    if (existingStyleTag) {
-      existingStyleTag.textContent = cssCode;
-    } else {
-      const newStyleTag = document.createElement('style');
-      newStyleTag.id = 'custom-css-modification';
-      newStyleTag.textContent = cssCode;
-      document.head.appendChild(newStyleTag);
-    }
-  } else {
-    if (existingStyleTag) {
-      existingStyleTag.remove();
-    }
-  }
-}
+  
+  // Load the saved settings when the DOM is ready
+  document.addEventListener('DOMContentLoaded', function () {
+    loadSavedSettings();
+  });
+  
