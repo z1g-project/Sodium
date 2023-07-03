3bf6823c8fccf717760c2ac52936167e6f1ade09
John Glynn <playingallday383@gmail.com>
2023-07-03 09:13:29 -0400

    Major Update

---
+++ ultraviolet-static/public/credits.js
@@ -0,0 +1,27 @@
+function toggleLicense(license) {
+    var licenseContent = document.getElementById(license + '-license');
+    licenseContent.style.display = licenseContent.style.display === 'none' ? 'block' : 'none';
+  }
+  
+  window.addEventListener('DOMContentLoaded', function() {
+    fetch('MIT.txt')
+      .then(response => response.text())
+      .then(text => {
+        var mitLicense = document.getElementById('mit-license');
+        mitLicense.textContent = text;
+      });
+  
+    fetch('GNU.txt')
+      .then(response => response.text())
+      .then(text => {
+        var gnuLicense = document.getElementById('gnu-license');
+        gnuLicense.textContent = text;
+      });
+    fetch('credits.txt')
+      .then(response => response.text())
+      .then(text => {
+        var creditsLicense = document.getElementById('credits-license');
+        creditsLicense.textContent = text;
+      });
+  });
+  
--- ultraviolet-static/public/time.js
+++ ultraviolet-static/public/time.js
@@ -1,41 +1,159 @@
-function updateBatteryStatus() {
-  var batteryElement = document.getElementById('battery');
-  
-  if ('getBattery' in navigator) {
-    navigator.getBattery().then(function(battery) {
-      if (battery.charging) {
-        batteryElement.textContent = 'Charging: ' + Math.floor(battery.level * 100) + '%';
-      } else {
-        batteryElement.textContent = '';
-      }
-    });
-  } else if ('battery' in navigator) {
-    var battery = navigator.battery || navigator.webkitBattery || navigator.mozBattery;
-    
-    if (battery.charging) {
-      batteryElement.textContent = 'Charging: ' + Math.floor(battery.level * 100) + '%';
+// Function to update battery icon
+function updateBatteryIcon(percentage, isCharging) {
+  const batteryElement = document.getElementById('battery');
+
+  if (isCharging) {
+    if (percentage <= 20) {
+      batteryElement.className = 'battery-icon charging low';
+    } else if (percentage <= 50) {
+      batteryElement.className = 'battery-icon charging medium';
     } else {
-      batteryElement.textContent = '';
+      batteryElement.className = 'battery-icon charging high';
     }
   } else {
-    batteryElement.textContent = '';
+    if (percentage <= 20) {
+      batteryElement.className = 'battery-icon low';
+    } else if (percentage <= 50) {
+      batteryElement.className = 'battery-icon medium';
+    } else {
+      batteryElement.className = 'battery-icon high';
+    }
   }
 }
 
-function updateTime() {
-  var date = new Date();
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'PM' : 'AM';
+function updateBatteryText(percentage, isCharging) {
+  const batteryTextElement = document.querySelector('.battery-text');
 
-  // Convert hours to 12-hour format
-  hours = hours % 12 || 12;
+  if (
+    batteryTextElement.classList.contains('low') ||
+    batteryTextElement.classList.contains('charging-low')
+  ) {
+    batteryTextElement.style.color = 'black';
+  } else {
+    batteryTextElement.style.color = 'white';
+  }
 
-  var timeElement = document.getElementById('time');
-  timeElement.textContent = 'Time: ' + hours + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + ampm;
+  const chargingSymbol = isCharging ? ' ⚡' : '';
+  batteryTextElement.textContent = `${percentage}%${chargingSymbol}`;
 }
 
-updateTime()
+function formatTime(time) {
+  return time < 10 ? '0' + time : time;
+}
 
-setInterval(updateBatteryStatus, 1000);
+function updateTime() {
+  const timeElement = document.getElementById('time');
+  const currentTime = new Date();
+  let hours = currentTime.getHours();
+  const minutes = formatTime(currentTime.getMinutes());
+  let timeString = '';
+
+  if (hours > 12) {
+    hours -= 12;
+    timeString = hours + ':' + minutes + ' PM';
+  } else {
+    if (hours === 0) {
+      hours = 12;
+    }
+    timeString = hours + ':' + minutes + ' AM';
+  }
+
+  timeElement.textContent = 'Time: ' + timeString;
+}
+
+// Get battery percentage and charging status
+function getBatteryInfo() {
+  if ('getBattery' in navigator) {
+    navigator.getBattery().then(function(battery) {
+      const batteryPercentage = Math.floor(battery.level * 100);
+      const isCharging = battery.charging;
+
+      updateBatteryIcon(batteryPercentage, isCharging);
+      updateBatteryText(batteryPercentage);
+    });
+  } else if ('battery' in navigator) {
+    const battery = navigator.battery || navigator.webkitBattery || navigator.mozBattery;
+    const batteryPercentage = Math.floor(battery.level * 100);
+    const isCharging = battery.charging;
+
+    updateBatteryIcon(batteryPercentage, isCharging);
+    updateBatteryText(batteryPercentage);
+  }
+}
+
+function updateConnectionStatus(connectionType) {
+  const connectionElement = document.getElementById('connection');
+
+  // Clear existing classes
+  connectionElement.className = 'internet-connection';
+
+  // Set appropriate class based on the connection type
+  if (connectionType === 'ethernet') {
+    connectionElement.classList.add('ethernet-connected');
+  } else if (connectionType === 'cell_none') {
+    connectionElement.classList.add('cell-none');
+  } else if (connectionType === 'cell_bad') {
+    connectionElement.classList.add('cell-bad');
+  } else if (connectionType === 'cell_low') {
+    connectionElement.classList.add('cell-low');
+  } else if (connectionType === 'cell_med') {
+    connectionElement.classList.add('cell-med');
+  } else if (connectionType === 'cell_good') {
+    connectionElement.classList.add('cell-good');
+  } else if (connectionType === 'cell_best') {
+    connectionElement.classList.add('cell-best');
+  } else if (connectionType === 'wifi_connected') {
+    connectionElement.classList.add('wifi-connected');
+  } else {
+    connectionElement.classList.add('wifi-unavailable');
+  }
+}
+
+// Function to determine the internet connection type
+function getConnectionType() {
+  // Use appropriate logic to determine the connection type
+  // Example: Check if Ethernet is connected
+  const isEthernetConnected = true;
+
+  // Example: Check if cellular connection is available and its quality
+  const isCellularAvailable = true;
+  const cellularSignalQuality = 'good'; // Can be 'none', 'bad', 'low', 'med', 'good', 'best'
+
+  // Example: Check if WiFi is connected
+  const isWifiConnected = false;
+
+  // Determine the connection type based on the conditions
+  let connectionType = '';
+  if (isEthernetConnected) {
+    connectionType = 'ethernet';
+  } else if (isCellularAvailable) {
+    if (cellularSignalQuality === 'none') {
+      connectionType = 'cell_none';
+    } else if (cellularSignalQuality === 'bad') {
+      connectionType = 'cell_bad';
+    } else if (cellularSignalQuality === 'low') {
+      connectionType = 'cell_low';
+    } else if (cellularSignalQuality === 'med') {
+      connectionType = 'cell_med';
+    } else if (cellularSignalQuality === 'good') {
+      connectionType = 'cell_good';
+    } else if (cellularSignalQuality === 'best') {
+      connectionType = 'cell_best';
+    }
+  } else if (isWifiConnected) {
+    connectionType = 'wifi_connected';
+  } else {
+    connectionType = 'wifi_unavailable';
+  }
+
+  return connectionType;
+}
+
+//const connectionType = getConnectionType();
+//updateConnectionStatus(connectionType); Removed for maintence
+
+getBatteryInfo();
+
+updateTime();
+
 setInterval(updateTime, 1000);
