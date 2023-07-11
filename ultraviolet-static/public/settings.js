e9920aa0d997b48ce6086113fb58e8919c00f8fc
John Glynn <playingallday383@gmail.com>
2023-07-11 16:49:31 -0400

    Add Backup Bare Servers + More Customizing (v1.0 RC2)

--- ultraviolet-static/public/settings.js
+++ ultraviolet-static/public/settings.js
@@ -1,133 +1,174 @@
-// Function to load and apply the saved settings
-function loadSavedSettings() {
-  // Load and set the title
-  const title = localStorage.getItem('websiteTitle');
-  if (title) {
-    document.title = title;
-    const websiteTitleElem = document.getElementById('website-title');
-    if (websiteTitleElem) {
-      websiteTitleElem.textContent = title;
+(function() {
+  // Function to load and apply the saved settings
+  function loadSavedSettings() {
+    // Load and set the title
+    const title = localStorage.getItem('websiteTitle');
+    if (title) {
+      document.title = title;
+      const websiteTitleElem = document.getElementById('website-title');
+      if (websiteTitleElem) {
+        websiteTitleElem.textContent = title;
+      }
     }
-  }
 
-  // Load and set the icon
-  const icon = localStorage.getItem('websiteIcon');
-  if (icon) {
-    const favicon = document.getElementById('favicon');
-    if (favicon) {
-      favicon.href = icon;
-      favicon.setAttribute('type', 'image/png');
+    // Load and set the icon
+    const icon = localStorage.getItem('websiteIcon');
+    if (icon) {
+      const favicon = document.getElementById('favicon');
+      if (favicon) {
+        favicon.href = icon;
+        favicon.setAttribute('type', 'image/png');
+      }
     }
-  }
 
-  // Load and apply the CSS
-  const css = localStorage.getItem('websiteCSS');
-  if (css) {
-    applyCSS(css);
-  }
+    // Load and apply the CSS
+    const css = localStorage.getItem('websiteCSS');
+    if (css) {
+      applyCSS(css);
+    }
 
-  // Load and set the search engine
-  const searchEngine = localStorage.getItem('searchEngine');
-  if (searchEngine) {
-    const searchEngineSelect = document.getElementById('search-engine-select');
-    if (searchEngineSelect) {
+    const searchEngine = localStorage.getItem("searchEngine");
+    const searchEngineSelect = document.getElementById("search-engine-select");
+
+    if (searchEngine) {
       searchEngineSelect.value = searchEngine;
     }
-  }
 
-  // Load and set the website title and icon inputs
-  const titleInput = document.getElementById('title-input');
-  const iconInput = document.getElementById('icon-input');
-  if (titleInput && iconInput) {
-    titleInput.value = title || '';
-    iconInput.value = icon || '';
-  }
-}
+    const bareServer = localStorage.getItem("bareServer");
+    const bareServerSelect = document.getElementById("bare-server-select");
 
-function applyCSS(selectedCSS) {
-  const styleSheets = document.getElementsByTagName('link');
+    if (bareServer) {
+      bareServerSelect.value = bareServer;
+    }
 
-  for (let i = 0; i < styleSheets.length; i++) {
-    const styleSheet = styleSheets[i];
-    if (styleSheet.getAttribute('id') === 'custom-css') {
-      styleSheet.href = selectedCSS;
+    // Load and set the website title and icon inputs
+    const titleInput = document.getElementById('title-input');
+    const iconInput = document.getElementById('icon-input');
+    if (titleInput && iconInput) {
+      titleInput.value = title || '';
+      iconInput.value = icon || '';
     }
   }
-}
 
-function saveWebsiteSettings() {
-  const titleInput = document.getElementById('title-input');
-  const iconInput = document.getElementById('icon-input');
+  function applyCSS(selectedCSS) {
+    const styleSheets = document.getElementsByTagName('link');
 
-  const title = titleInput.value;
-  const icon = iconInput.value;
-
-  const toggleBeta = document.getElementById('toggle-beta');
-  const betaMode = toggleBeta.checked;
-
-  // Save the settings in localStorage
-  localStorage.setItem('websiteTitle', title);
-  localStorage.setItem('websiteIcon', icon);
-  localStorage.setItem('betaMode', betaMode ? 'true' : 'false');
-
-  // Apply the saved settings
-  document.title = title;
-  const websiteTitleElem = document.getElementById('website-title');
-  if (websiteTitleElem) {
-    websiteTitleElem.textContent = title;
-  }
-  const favicon = document.getElementById('favicon');
-  if (favicon) {
-    favicon.href = icon;
-    favicon.setAttribute('type', 'image/png');
+    for (let i = 0; i < styleSheets.length; i++) {
+      const styleSheet = styleSheets[i];
+      if (styleSheet.getAttribute('id') === 'custom-css') {
+        styleSheet.href = selectedCSS;
+      }
+    }
   }
 
-  console.log('Settings saved:');
-  console.log('Title:', title);
-  console.log('Icon:', icon);
-  console.log('Beta Mode:', betaMode);
-}
+  // Function to save the settings
+  function saveSettings() {
+    // Save the title
+    const titleInput = document.getElementById("title-input");
+    const title = titleInput.value.trim();
+    localStorage.setItem("websiteTitle", title);
+    console.log("Title saved:", title);
 
-// Load the saved settings when the DOM is ready
-document.addEventListener('DOMContentLoaded', function () {
+    // Save the icon
+    const iconInput = document.getElementById("icon-input");
+    const icon = iconInput.value.trim();
+    localStorage.setItem("websiteIcon", icon);
+    console.log("Icon saved:", icon);
+
+    const searchEngineSelect = document.getElementById("search-engine-select");
+    const searchEngine = searchEngineSelect.value;
+
+    if (searchEngine === "custom") {
+      const customSearchUrl = document.getElementById("custom-search-engine-input").value.trim();
+
+      if (customSearchUrl !== "") {
+        localStorage.setItem("searchEngine", customSearchUrl);
+        console.log("Search engine saved:", customSearchUrl);
+      }
+    } else {
+      localStorage.setItem("searchEngine", searchEngine);
+      console.log("Search engine saved:", searchEngine);
+    }
+
+    const bareServerSelect = document.getElementById("bare-server-select");
+    const bareServer = bareServerSelect.value;
+
+    if (bareServer === "custom") {
+      const customBareServerUrl = document.getElementById("custom-bare-server-input").value.trim();
+
+      if (customBareServerUrl !== "") {
+        localStorage.setItem("bareServer", customBareServerUrl);
+        console.log("Bare server saved:", customBareServerUrl);
+      }
+    } else {
+      localStorage.setItem("bareServer", bareServer);
+      console.log("Bare server saved:", bareServer);
+    }
+  }
+
+  // Load the saved settings when the script is executed
   loadSavedSettings();
-});
 
-const applyCSSButton = document.getElementById('apply-css-button');
-if (applyCSSButton) {
-  applyCSSButton.addEventListener('click', function () {
-    const cssSelect = document.getElementById('css-select');
-    const selectedCSS = cssSelect.value;
-    applyCSS(selectedCSS);
+  const applyCSSButton = document.getElementById('apply-css-button');
+  if (applyCSSButton) {
+    applyCSSButton.addEventListener('click', function () {
+      const cssSelect = document.getElementById('css-select');
+      const selectedCSS = cssSelect.value;
+      applyCSS(selectedCSS);
 
-    // Save the selected CSS in localStorage
-    localStorage.setItem('websiteCSS', selectedCSS);
-  });
-}
-
-const saveButton = document.getElementById('save-button');
-if (saveButton) {
-  saveButton.addEventListener('click', function () {
-    saveWebsiteSettings();
-  });
-}
-
-function handleToggleAboutBlank() {
-  const toggleAboutBlank = document.getElementById('open-new-window');
-
-  if (toggleAboutBlank.checked) {
-    // Set the state in localStorage to indicate about:blank mode
-    localStorage.setItem('openNewWindow', 'true');
-  } else {
-    // Remove the state from localStorage
-    localStorage.removeItem('openNewWindow');
+      // Save the selected CSS in localStorage
+      localStorage.setItem('websiteCSS', selectedCSS);
+    });
   }
-}
 
-// Event listener for the "Toggle about:blank" checkbox
-const toggleAboutBlank = document.getElementById('open-new-window');
-if (toggleAboutBlank) {
-  toggleAboutBlank.addEventListener('change', function () {
-    handleToggleAboutBlank();
+  const saveButton = document.getElementById('save-button');
+  if (saveButton) {
+    saveButton.addEventListener('click', function () {
+      saveSettings();
+    });
+  }
+
+  function handleToggleAboutBlank() {
+    const toggleAboutBlank = document.getElementById('open-new-window');
+
+    if (toggleAboutBlank.checked) {
+      // Set the state in localStorage to indicate about:blank mode
+      localStorage.setItem('openNewWindow', 'true');
+    } else {
+      // Remove the state from localStorage
+      localStorage.removeItem('openNewWindow');
+    }
+  }
+
+  // Event listener for the "Toggle about:blank" checkbox
+  const toggleAboutBlank = document.getElementById('open-new-window');
+  if (toggleAboutBlank) {
+    toggleAboutBlank.addEventListener('change', function () {
+      handleToggleAboutBlank();
+    });
+  }
+
+  // Event listeners for custom search engine and custom bare server inputs
+  const searchEngineSelect = document.getElementById('search-engine-select');
+  const customSearchEngineInput = document.getElementById('custom-search-engine-input');
+
+  searchEngineSelect.addEventListener('change', function () {
+    if (searchEngineSelect.value === 'custom') {
+      customSearchEngineInput.style.display = 'block';
+    } else {
+      customSearchEngineInput.style.display = 'none';
+    }
   });
-}
+
+  const bareServerSelect = document.getElementById('bare-server-select');
+  const customBareServerInput = document.getElementById('custom-bare-server-input');
+
+  bareServerSelect.addEventListener('change', function () {
+    if (bareServerSelect.value === 'custom') {
+      customBareServerInput.style.display = 'block';
+    } else {
+      customBareServerInput.style.display = 'none';
+    }
+  });
+
+})();
--- ultraviolet-static/public/uv/uv.config.js
+++ ultraviolet-static/public/uv/uv.config.js
@@ -1,13 +1,46 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
-    prefix: "/uv/service/",
-    bare: "https://uv.studentportal.lol/",
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    client: "/uv/uv.client.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
-  };
+  prefix: "/uv/service/",
+  bare: [
+    "https://uv.studentportal.lol/",
+    "https://uv.radon.games/",
+    "https://uv.holyubofficial.net/",
+    "https://bare-server-repl.johnglynn2.repl.co/",
+  ],
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
+};
+
+function customBareServer() {
+  const cookieName = "bareServer";
+  const localStorageKey = "bareServer";
+
+  // Check if the custom bare server URL exists in cookies
+  const cookieValue = getCookie(cookieName);
+
+  if (cookieValue) {
+    // Custom bare server URL found in cookies
+    return cookieValue;
+  } else {
+    // Fallback to the value in localStorage
+    const localStorageValue = localStorage.getItem(localStorageKey);
+    return localStorageValue || "https://uv.studentportal.lol/";
+  }
+}
+
+function getCookie(name) {
+  const cookies = document.cookie.split(";");
+
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(name + "=")) {
+      return cookie.substring(name.length + 1);
+    }
+  }
+
+  return null;
+}
