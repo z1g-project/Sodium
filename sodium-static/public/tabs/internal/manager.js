8dde360ebf8ddab4c69d43903112df52f38e1b6c
John Glynn <playingallday383@gmail.com>
2023-08-22 16:20:00 -0400

    fixes

--- sodium-static/public/tabs/internal/manager.js
+++ sodium-static/public/tabs/internal/manager.js
@@ -1,8 +1,3 @@
 const internal_pages = {
-    // YOU MUST ADD YOUR INTERNAL PAGE TO THIS FILE IF YOU WANT IT TO WORK!
-    // This index is how HT can reference what page to what file
-    newtab: "../newtab.html",
-    //extensionsmarketplace: "/internal/extensions/marketplace.html",
-    //extensions: "/internal/extensions/index.html",
-    //games: "/internal/g/index.html"
+   newtab: "../newtab.html",
 }
