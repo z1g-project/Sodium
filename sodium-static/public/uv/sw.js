25756768b30e610114c368759f97e6a306fec9e1
John Glynn <playingallday383@gmail.com>
2023-08-29 12:09:59 -0400

    Fix SW Scope Errors

--- sodium-static/public/uv/sw.js
+++ sodium-static/public/uv/sw.js
@@ -11,4 +11,32 @@ importScripts(__uv$config.sw || 'uv.sw.js');
 
 const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+let bareServerUrl;
+
+async function updateBareServerUrl() {
+  const cache = await caches.open('bareServerCache');
+  const response = await cache.match('bareServerKey');
+  if (response) {
+    bareServerUrl = await response.text();
+  }
+}
+
+updateBareServerUrl();
+
+self.addEventListener('fetch', async (event) => {
+  if (bareServerUrl) {
+    const req = new Request(bareServerUrl, {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'same-origin',
+    });
+
+    const res = await fetch(req);
+
+    if (self.__uv$config) {
+      self.__uv$config.bare = res.url;
+    }
+  }
+
+  event.respondWith(sw.fetch(event));
+});
