cd2f431971e82d870d89fcbd68ba351f576447ee
John Glynn <playingallday383@gmail.com>
2023-08-29 12:06:59 -0400

    Fixes

--- sodium-static/public/uv/uv.config.js
+++ sodium-static/public/uv/uv.config.js
@@ -1,12 +1,11 @@
-/*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/service/',
-    bare: '/bare/',
+    prefix: "/uv/service/",
+    bare: "https://uv.holyubofficial.net",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv.handler.js',
-    client: '/uv.client.js',
-    bundle: '/uv.bundle.js',
-    config: '/uv.config.js',
-    sw: '/uv.sw.js',
-};
+    handler: "/uv/uv.handler.js",
+    client: "/uv/uv.client.js",
+    bundle: "/uv/uv.bundle.js",
+    config: "/uv/uv.config.js",
+    sw: "/uv/uv.sw.js",
+  };
