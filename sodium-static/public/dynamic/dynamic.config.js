5028c96c34bbff8c86cc73b4b2080ae50dec1665
John <72810050+Notplayingallday383@users.noreply.github.com>
2023-09-04 13:04:01 -0400

    Switch Bare Servers

--- sodium-static/public/dynamic/dynamic.config.js
+++ sodium-static/public/dynamic/dynamic.config.js
@@ -5,7 +5,7 @@ self.__dynamic$config = {
   logLevel: 3,
   bare: {
     version: 2,
-    path: "https://uv.holyubofficial.net"
+    path: "https://tomp.app"
   },
   tab: {
     title: 'Service',
--- sodium-static/public/uv/uv.config.js
+++ sodium-static/public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: "/uv/service/",
-    bare: "https://uv.holyubofficial.net",
+    bare: "https://tomp.app",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/uv/uv.handler.js",
