21112b804d3fd1a3837acf7caa79fbd42322ea3d
John <72810050+Notplayingallday383@users.noreply.github.com>
2023-09-07 06:25:06 -0400

    Switch Bare Servers (Fix to weird TypeError)

--- sodium-static/public/uv/uv.config.js
+++ sodium-static/public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: "/uv/service/",
-    bare: "https://tomp.app",
+    bare: "https://sodium.onrender.com/bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/uv/uv.handler.js",
