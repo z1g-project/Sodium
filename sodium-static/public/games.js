41d341a83c3dfe5b120f8b98e8b6a737f3e0249b
John Glynn <playingallday383@gmail.com>
2023-09-16 19:25:19 -0400

    Experimental UI

--- sodium-static/public/games.js
+++ sodium-static/public/games.js
@@ -1,71 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const gamesData = [
-    {
-      title: 'EaglerCraft',
-      category: 'Featured',
-      description: 'A popular game during school hours (Multiple Versions)',
-      imageUrl: 'games/assets/game1.jpg',
-      url: 'games/eagler/',
-    },
-    {
-      title: 'Suprem.io',
-      category: 'Online Games',
-      description: 'A popular game during school hours (Multiple Versions)',
-      imageUrl: 'games/assets/game1.jpg',
-      url: 'games/eagler/',
-    },
-  ];
+function showImages() {
+  var selectedCategories = Array.from(document.querySelectorAll("#category option:checked")).map(option => option.value);
+  var games = document.getElementsByClassName("column");
 
-  function createGameCard(gameData) {
-    const card = document.createElement('div');
-    card.classList.add('game-card');
+  for (var i = 0; i < games.length; i++) {
+    var game = games[i];
+    var categories = game.getAttribute("data-category").split(" ");
 
-    const image = document.createElement('img');
-    image.src = gameData.imageUrl;
-    image.alt = gameData.title;
-
-    const title = document.createElement('h3');
-    title.textContent = gameData.title;
-
-    const description = document.createElement('p');
-    description.textContent = gameData.description;
-
-    const playButton = document.createElement('button');
-    playButton.classList.add('play-button');
-    playButton.textContent = 'Play';
-
-    card.appendChild(image);
-    card.appendChild(title);
-    card.appendChild(description);
-    card.appendChild(playButton);
-
-    card.addEventListener('click', () => {
-      loadGame(gameData.url);
-    });
-
-    console.log('Card Returned');
-    return card;
+    if (selectedCategories.length === 0 || selectedCategories.some(category => categories.includes(category))) {
+      game.style.display = "block";
+    } else {
+      game.style.display = "none";
+    }
   }
+}
 
-  function populateGameSections() {
-    const gameSections = {
-      'Featured': document.querySelector('#featured-section .grid-container'),
-      'Web Games': document.querySelector('#web-games-section .grid-container'),
-      'Online Games': document.querySelector('#online-games-section .grid-container'),
-      'Flash Games': document.querySelector('#flash-games-section .grid-container'),
-    };
+function searchgames() {
+  var input = document.getElementById("gamesearch");
+  var filter = input.value.toLowerCase();
+  var games = document.getElementsByClassName("column");
 
-    gamesData.forEach((gameData) => {
-      const card = createGameCard(gameData);
-      const section = gameSections[gameData.category];
+  for (var i = 0; i < games.length; i++) {
+    var game = games[i];
+    var name = game.getElementsByTagName("p")[0].textContent.toLowerCase();
 
-      if (section) {
-        section.appendChild(card);
-        console.log('Appended Section');
-      }
-    });
+    if (name.includes(filter)) {
+      game.style.display = "block";
+    } else {
+      game.style.display = "none";
+    }
   }
-
-  populateGameSections();
-  console.log('Game Sections Populated');
-});
+}
