4114054ed26919d63f3f5e7c6df239b4633f6baa
John <72810050+Notplayingallday383@users.noreply.github.com>
2023-11-09 06:30:35 -0500

    Updates Again

--- sodium-static/public/regcache.js
+++ sodium-static/public/regcache.js
@@ -1,7 +1,17 @@
-navigator.serviceWorker.register('cache.js')
+if (window.location.hostname.includes('.pages.dev')) {
+  navigator.serviceWorker.register('static-cache.js')
+    .then((registration) => {
+      console.log('Static Cache Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Error registering Service Worker:', error);
+    });
+} else {
+  navigator.serviceWorker.register('cache.js')
     .then((registration) => {
       console.log('Cache Service Worker registered with scope:', registration.scope);
     })
     .catch((error) => {
       console.error('Error registering Service Worker:', error);
     });
+}
