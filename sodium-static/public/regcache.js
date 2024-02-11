2fdcf9358d8338d26aa97eab700b52103cb6c638
John <playingallday383@gmail.com>
2024-02-11 10:40:36 -0500

    I might be onto something

--- sodium-static/public/regcache.js
+++ sodium-static/public/regcache.js
@@ -1,6 +1,6 @@
 function updateCache() {
   if (navigator.onLine) {
-      console.log('%c Sodium Cache',`background: #16d495; color: white; font-weight: bold; border-radius: 5px;`, '' + 'Updating Cache Please Wait...')
+      console.log('%c Sodium Cache ',`background: #16d495; color: white; font-weight: bold; border-radius: 5px;`, '' + 'Updating Cache Please Wait...')
       regCache()
   } else {
       console.log('Your Offline! Cache wont be updated untill your online')
@@ -11,18 +11,18 @@ function regCache() {
   if (window.location.hostname.includes('.pages.dev')) {
     navigator.serviceWorker.register('static-cache.js')
       .then((registration) => {
-        console.log('%c Sodium Cache',`background: #16d495; color: white; font-weight: bold; border-radius: 5px;`, '' + 'Static Cache Service Worker registered with scope:', registration.scope);
+        console.log('%c Sodium Cache ',`background: #16d495; color: white; font-weight: bold; border-radius: 5px;`, '' + 'Static Cache Service Worker registered with scope:', registration.scope);
       })
       .catch((error) => {
-        console.error('%c Sodium Cache',`background: #16d495; color: white; font-weight: bold; border-radius: 5px;`, '' + 'Error registering Service Worker:', error);
+        console.error('%c Sodium Cache ',`background: #16d495; color: white; font-weight: bold; border-radius: 5px;`, '' + 'Error registering Service Worker:', error);
       });
   } else {
     navigator.serviceWorker.register('cache.js')
       .then((registration) => {
-        console.log('%c Sodium Cache',`background: #16d495; color: white; font-weight: bold; border-radius: 5px;`, '' + 'Cache Service Worker registered with scope:', registration.scope);
+        console.log('%c Sodium Cache ',`background: #16d495; color: white; font-weight: bold; border-radius: 5px;`, '' + 'Cache Service Worker registered with scope:', registration.scope);
       })
       .catch((error) => {
-        console.error('%c Sodium Cache',`background: #16d495; color: white; font-weight: bold; border-radius: 5px;`, '' + 'Error registering Service Worker:', error);
+        console.error('%c Sodium Cache ',`background: #16d495; color: white; font-weight: bold; border-radius: 5px;`, '' + 'Error registering Service Worker:', error);
       });
   }
 }
