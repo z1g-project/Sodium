49c5ed7f6e5de2bfdfd3a2408d22d774c57ca905
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-02-27 14:27:49 -0500

    Ill fix this when I get home

--- components/modules/settings.ts
+++ components/modules/settings.ts
@@ -1,5 +1,78 @@
 export default async function loadSettings() {
-   
+  const title = localStorage.getItem('websiteTitle');
+  const icon = localStorage.getItem('websiteIcon');
+  const useRandomize = localStorage.getItem('useRandomizer')
+  const css = localStorage.getItem('websiteCSS');
+  const metaThemeColor = localStorage.getItem('metaThemeColor');
+  const iconInput = document.getElementById('icon-input');
+  const customCSS = localStorage.getItem('websiteCSS');
+  if (title) {
+    document.title = title;
+  }
+  const icon = localStorage.getItem('websiteIcon');
+  if (icon) {
+    const favicon = document.getElementById('favicon');
+    if (favicon) {
+      favicon.href = icon;
+      favicon.setAttribute('type', 'image/png');
+    }
+  }
+  if (useRandomize) {
+    const titles = ['Google', 'Google Classroom', 'SchoolTube', 'Kahoot', 'Bing Images', 'Microsoft Word', 'Google Docs', 'Microsoft Excel', 'Google Account', 'about:blank', 'Google Maps', 'Google Drive', 'gmail', 'Outlook Web'];
+    const favicons = ['https://www.google.com/favicon.ico', 'https://cdn.z1g-project.pages.dev/', 'https://www.microsoft.com/favicon.ico', 'https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png', 'https://kahoot.it/favicon.ico', 'https://www.outlook.com/owa/favicon.ico'];
+    const randomTitleIndex = Math.floor(Math.random() * titles.length);
+    const randomFaviconIndex = Math.floor(Math.random() * favicons.length);
+    document.title = titles[randomTitleIndex];
+    const faviconLink = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    faviconLink.type = 'image/x-icon';
+    faviconLink.rel = 'icon';
+    faviconLink.href = favicons[randomFaviconIndex];
+    const head = document.head || document.getElementsByTagName('head')[0];
+    head.appendChild(faviconLink);
+    console.log('Randomized!')
+  }
+  if (css) {
+    applyCSS(css);
+    const cssSelect = document.getElementById('css-select');
+    if (cssSelect) {
+      for (let i = 0; i < cssSelect.options.length; i++) {
+        if (cssSelect.options[i].value === css) {
+          cssSelect.selectedIndex = i;
+          break;
+        }
+      }
+    }
+  }
+  if (customCSS) {
+    if (window.location.pathname.includes('css-editor.html')) {
+      const styleSheet = document.createElement('style');
+      styleSheet.id = 'custom-css';
+      styleSheet.textContent = customCSS;
+      document.head.appendChild(styleSheet);
+    } else {
+      applyCSS(customCSS);
+    }
+  } else {
+    const defaultStyleSheet = document.createElement('link');
+    defaultStyleSheet.rel = 'stylesheet';
+    defaultStyleSheet.href = 'assets/css/ui.css';
+    defaultStyleSheet.id = 'custom-css';
+    document.head.appendChild(defaultStyleSheet);
+  }
+  const metaThemeColor = localStorage.getItem('metaThemeColor');
+  if (metaThemeColor) {
+    document.getElementById('meta-theme-color').value = metaThemeColor;
+    const metaThemeColorTag = document.querySelector('meta[name="theme-color"]');
+    if (metaThemeColorTag) {
+      metaThemeColorTag.setAttribute('content', metaThemeColor);
+    }
+  }
+  if (metaThemeColor) {
+    const metaThemeColorTag = document.querySelector('meta[name="theme-color"]');
+    if (metaThemeColorTag) {
+      metaThemeColorTag.setAttribute('content', metaThemeColor);
+    }
+  }  
 }
 
 export async function saveSettings() {
@@ -187,5 +260,125 @@ export async function importSettings() {
 }
 
 export async function getSettings() {
-    
+  const websiteTitleElem = document.getElementById('website-title');
+  const toggleBeta = document.getElementById('toggle-beta');
+  const bareServerInput = document.getElementById('custom-bare-server-input');
+  const titleInput = document.getElementById('title-input');
+  const emergencyHotkey = localStorage.getItem('emergencyHotkey');
+  const emergencyHotkeyInput = document.getElementById('emergency-switch-hotkey');
+  const proxyOption = localStorage.getItem('proxyOption');
+  const proxySelect = document.getElementById('proxySelect');
+  const options = proxySelect.options;
+  const emergencyURL = localStorage.getItem('emergencyURL');
+  const emergencyURLInput = document.getElementById('emergency-url-input');
+  const openNewWindow = localStorage.getItem('openNewWindow');
+  const toggleAboutBlank = document.getElementById('open-new-window');
+  const openblobWindow = localStorage.getItem('openblobwindow');
+  const toggleblobBlank = document.getElementById('open-blob-window');
+  const titlerandomizerls = localStorage.getItem('useRandomizer');
+  const titlerandomizer = document.getElementById('title-randomizer');
+  const debugging = localStorage.getItem('debugging');
+  const toggleDebugging = document.getElementById('toggle-debugging');
+  const fallbackUrl = localStorage.getItem('fallbackUrl');
+  const fallbackUrlInput = document.getElementById('fallback-url-input');
+  const searchEngine = localStorage.getItem('searchEngine');
+  const searchEngineSelect = document.getElementById('search-engine-select');
+  const customSearchEngineInput = document.getElementById('custom-search-engine-input');
+  const use24HourTimeCheckbox = document.getElementById('use-24hour-checkbox');
+  const includeDateCheckbox = document.getElementById('include-date-checkbox');
+  const useSecondsCheckbox = document.getElementById('use-seconds-checkbox');
+  const clockCheckbox = document.getElementById('disable-clock');
+  if (websiteTitleElem) {
+    websiteTitleElem.textContent = title;
+  }
+  if (toggleBeta) {
+    const betaMode = localStorage.getItem('betaMode');
+    toggleBeta.checked = betaMode === 'true';
+  }
+  if (bareServerInput) {
+    bareServerInput.value = localStorage.getItem('bareServer') || '';
+  }
+  if (titleInput && iconInput) {
+    titleInput.value = title || '';
+    iconInput.value = icon || '';
+  }
+  if (emergencyHotkey && emergencyHotkeyInput) {
+    emergencyHotkeyInput.value = emergencyHotkey;
+  }
+  if (emergencyHotkeyInput) {
+    emergencyHotkeyInput.addEventListener('click', function (event) {
+      event.preventDefault();
+      document.addEventListener('keydown', function (keyEvent) {
+        keyEvent.preventDefault();
+        emergencyHotkeyInput.value = keyEvent.key.toLowerCase();
+        document.removeEventListener('keydown', arguments.callee);
+      });
+    });
+  }
+  if (emergencyURL && emergencyURLInput) {
+    emergencyURLInput.value = emergencyURL;
+  }
+  if (toggleAboutBlank) {
+    toggleAboutBlank.checked = openNewWindow === 'true';
+  }
+  if (toggleblobBlank) {
+    toggleblobBlank.checked = openblobWindow === 'true';
+  }
+  if (titlerandomizer) {
+    titlerandomizer.checked = titlerandomizerls === 'true';
+  }
+  if (toggleDebugging) {
+    toggleDebugging.checked = debugging === 'true';
+  }
+  if (fallbackUrl && fallbackUrlInput) {
+    fallbackUrlInput.value = fallbackUrl;
+  }
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].value === proxyOption) {
+      options[i].selected = true;
+      break;
+    }
+  }
+  if (searchEngineSelect) {
+    searchEngineSelect.value = searchEngine;
+    if (searchEngine === 'custom') {
+      if (customSearchEngineInput) {
+        customSearchEngineInput.style.display = 'block';
+        customSearchEngineInput.value = localStorage.getItem('customSearchEngineUrl') || '';
+      }
+    }
+  }
+  if (use24HourTimeCheckbox) {
+    const use24HourTime = localStorage.getItem('use24HourTime');
+    use24HourTimeCheckbox.checked = use24HourTime === 'true';
+  }
+  if (includeDateCheckbox) {
+    const includeDate = localStorage.getItem('showDate');
+    includeDateCheckbox.checked = includeDate === 'true';
+  }
+  if (useSecondsCheckbox) {
+    const useSeconds = localStorage.getItem('useSeconds');
+    useSecondsCheckbox.checked = useSeconds === 'true';
+  }
+  if (clockCheckbox) {
+    const useSeconds = localStorage.getItem('noClock');
+    clockCheckbox.checked = useSeconds === 'true';
+  }  
+}
+
+function applyCSS() {
+  console.log('applyCSS Will be deprecated soon. This is simply a placeholder for the rewrite')
+  const styleSheets = document.getElementsByTagName('link');
+    for (let i = 0; i < styleSheets.length; i++) {
+      const styleSheet = styleSheets[i];
+      if (styleSheet.getAttribute('id') === 'custom-css') {
+        styleSheet.href = selectedCSS;
+      }
+    }
+    const saveButton = document.getElementById('save-button');
+    if (saveButton) {
+      saveButton.addEventListener('click', function () {
+        saveSettings();
+      });
+    }
 }
