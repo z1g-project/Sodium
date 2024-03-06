5f3bd0c7a0e3861ee9a1a577cbd3e38b3a913f8f
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-03-06 10:22:51 -0500

    Update ultra.config.js

--- sodium-static/public/ultra/ultra.config.js
+++ sodium-static/public/ultra/ultra.config.js
@@ -11,7 +11,7 @@ self.__uv$config = {
      * @type {string}
      */
     bare: [
-        "https://phantomnetwork.cloud/bare/"
+        "https://tomp.app"
         // Using a backend? Use this instead of Tomp.app:
         // "/bare1/",
         // "/bare2/",
