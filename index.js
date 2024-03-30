c57254d7e3dd25f5288243ad2d7fd0a1eb69bc4d
incognitotgt <tgt@incognitotgt.me>
2024-03-30 18:33:09 -0400

    fetch apps from api

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { createBareServer } from "@tomphttp/bare-server-node";
+import wisp from "wisp-server-node";
 import express from "express";
 import cors from "cors";
 import { createServer } from "node:http";
 import { publicPath } from "ultraviolet-static";
 import { join } from "node:path";
 import { hostname } from "node:os";
-import fs from 'fs';
-import ip from 'ip';
+import fs from "fs";
+import ip from "ip";
 
 let port = parseInt(process.env.PORT || "");
 if (isNaN(port)) port = 8080;
@@ -15,15 +16,17 @@ const barePaths = ["/bare1/", "/bare2/", "/bare3/"];
 const bareServers = barePaths.map((path) => createBareServer(path));
 const app = express();
 
-app.use(cors({
-  origin: `https://localhost:${port}`,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: `https://localhost:${port}`,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  }),
+);
 
 app.use(express.static(publicPath));
 
-app.use((req, res) => {
+app.use((_req, res) => {
   res.status(404);
   res.sendFile(join(publicPath, "404.html"));
 });
@@ -52,24 +55,30 @@ server.on("upgrade", (req, socket, head) => {
 
 server.on("listening", async () => {
   try {
-    const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+    const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
     const localVersion = packageJson.version;
-    const versionTxt = fs.readFileSync('./sodium-static/public/version.txt', 'utf8').trim();
+    const versionTxt = fs
+      .readFileSync("./sodium-static/public/version.txt", "utf8")
+      .trim();
 
     if (localVersion !== versionTxt) {
-      console.log('\x1b[32m[Sodium] Update is available ' + versionTxt + '. Check the GitHub for more info.\x1b[0m');
+      console.log(
+        "\x1b[32m[Sodium] Update is available " +
+          versionTxt +
+          ". Check the GitHub for more info.\x1b[0m",
+      );
       startServer();
     } else {
-      console.log('\x1b[32m[Sodium] Your up to date!\x1b[0m');
+      console.log("\x1b[32m[Sodium] Your up to date!\x1b[0m");
       startServer();
     }
   } catch (error) {
-    console.error('\x1b[31mError checking for updates:', error, '\x1b[0m');
+    console.error("\x1b[31mError checking for updates:", error, "\x1b[0m");
     startServer();
   }
 });
 
-function startServer() { 
+function startServer() {
   const address = server.address();
   const ipAddress = ip.address();
   console.log("Sodium is running on:");
@@ -90,7 +99,7 @@ process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
 
 function shutdown() {
-  console.log("SIGTERM signal received: closing HTTP server");
+  console.log("\nSIGTERM signal received: closing HTTP server");
   server.close();
   bareServers.forEach((bare) => bare.close());
   process.exit(0);
